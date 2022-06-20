dacbcec7d8a43bb21df9f8129c756edc5546174b
strongbaass <ggg266182@gmail.com>
2022-06-19 23:05:13 -0400

    fix #1

--- src/components/UI/NavBar/NavBar.jsx
+++ src/components/UI/NavBar/NavBar.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, {useRef} from "react";
 import '../../styles/NavBar.scss';
 import logo from '../../../img/logo.png';
 import bars from '../../../img/svg/barssolid.svg';
 import NavButtons from "../NavButtons/NavButtons";
 import BusinessHelp from "../../BusinessHelp";
-import '../../../js/navbar'
+import showtest from '../../../js/navbar'
+import closebtn from '../../../img/svg/xmark.svg';
 
 
 const NavBar = () => {
+    const navRef = useRef();
+
+    const showNavBar = () => {
+        navRef.current.classList.toggle('test')
+    }
+    const closeNavBar = () => {
+        navRef.current.classList.remove('test')
+    }
     return(
-            <header className="header">
+            <header className="header" >
             <div className="header_background">
                 <nav className="nav_menu">
                     <img src={logo} className="logo" alt=""/>
-                        <NavButtons/>
-                    <img src={bars} className="bars" id="bars" />
+                        <div className="nav_buttons" ref={navRef}>
+                            <img src={closebtn} className="closebtn" onClick={closeNavBar}/>
+                                <li className="nav_button">Product</li>
+                                <li className="nav_button">Soltuion</li>
+                                <li className="nav_button">Customer</li>
+                                <li className="nav_button">Pricing</li>
+                                <li className="nav_button">About us</li>  
+            <div className="auth_btns">
+                <button className="login_btn">Login </button>
+                <button className="signup_btn">Sign Up</button>
+            </div>  
+        </div>
+                    <img src={bars} className="bars" id="bars" onClick={showNavBar}/>
                 </nav>
                
                     <div className="header_content">
--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,17 +1,35 @@
-const navMenu = document.getElementById('bars');
-const closebtn = document.getElementById('closebtn')
-const navbuttons = document.getElementById('nav_buttons')
+// let navMenu = document.getElementById('bars');
+// let closebtn = document.getElementById('closebtn')
+// let navbuttons = document.getElementById('nav_buttons')
 
-const showNav = ('click', () => {
-    navbuttons.classList.add('test')
-})
-const closeNav = ('click' , () => {
-    navbuttons.classList.remove('test')
-})
-if(navMenu){
-    navMenu.addEventListener('click', showNav)
-}   
-if(closebtn){
-    closebtn.addEventListener('click',  closeNav )
-}
 
+// const showNav = ('click', () => {
+//     navbuttons.classList.toggle('test')
+// })
+// const closeNav = ('click' , () => {
+//     navbuttons.classList.toggle('test')
+// })
+
+
+
+
+// function showtest (){
+// if(navMenu.click){
+//     navMenu.addEventListener('click', showNav)
+//    return showNav;
+// }else{
+
+// }
+// }
+
+// if(closebtn){
+//     closebtn.addEventListener('click',  closeNav )
+// }
+// function showtest () {
+// useEffect(() => {
+//     setTimeout(() =>{
+//         navbuttons.current.classList.add
+//         })
+//     })
+// }
+// export default showtest;
